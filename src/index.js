564357c094302214a49def5a94a45d257fff5e1e
a-urban <aurban@abaenglish.com>
2021-02-03 11:59:00 +0100

    Tech Challenge

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/_main.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
