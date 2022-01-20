326b76dedb850c5eda1735f91d689653ead85bc5
Oriol Serra <87442357+byoriolserra@users.noreply.github.com>
2022-01-20 16:32:54 +0100

    Create Header component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
+import Header from '../Header/Header';
 
 const App = () => {
     return (
         <div>
             <Navbar />
+            <Header />
         </div>
     )
 };
--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 import Navbar from '../Navbar/Navbar';
+import Header from '../Header/Header';
 
 describe('App', () => {
     
@@ -12,12 +13,18 @@ describe('App', () => {
 
     it('renders without crashing', () => {
         expect(wrapper).toHaveLength(1);
-    })
+    });
 
     it('renders the navbar', () => {
         const navbar = wrapper.find(Navbar);
 
         expect(navbar).toHaveLength(1);
+    });
+
+    it('renders the header', () => {
+        const header = wrapper.find(Header);
+
+        expect(header).toHaveLength(1);
     })
 
 });
