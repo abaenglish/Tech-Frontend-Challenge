e6bc4ad0858b33979e621dd9d0d5121cb34e1c59
Oriol Serra <87442357+byoriolserra@users.noreply.github.com>
2022-01-20 12:37:44 +0100

    Switch styling to Sass and style Navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import avatar from '../../assets/images/avatar.svg';
+import '../../assets/css/main.css';
+// MUI ICONS
+import HeadphonesIcon from '@mui/icons-material/Headphones';
+import SearchIcon from '@mui/icons-material/Search';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import PlaylistPlayIcon from '@mui/icons-material/PlaylistPlay';
 
 const Navbar = () => {
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <div id='profile-picture'>
+                <img src={avatar} alt='Profile avatar' />
+            </div>
+            <div id='nav-links'>
+                <ul>
+                    <li><HeadphonesIcon sx={{ fontSize: 20 }} /></li>
+                    <li><SearchIcon sx={{ fontSize: 20 }} /></li>
+                    <li><FavoriteIcon sx={{ fontSize: 20 }} /></li>
+                    <li><PlayCircleIcon sx={{ fontSize: 20 }} /></li>
+                    <li><PlaylistPlayIcon sx={{ fontSize: 20 }} /></li>
+                </ul>
+            </div>
+        </nav>
     )
 };
 
--- src/components/Navbar/Navbar.test.js
+++ src/components/Navbar/Navbar.test.js
@@ -11,6 +11,18 @@ describe('Navbar', () => {
 
     it('renders without crashing', () => {
         expect(wrapper).toHaveLength(1);
+    });
+
+    it('renders a profile picture', () => {
+        const picture = wrapper.find('#profile-picture');
+
+        expect(picture).toHaveLength(1);
+    });
+
+    it('renders navigation links', () => {
+        const links = wrapper.find('#nav-links');
+
+        expect(links).toHaveLength(1);
     })
 
 });
