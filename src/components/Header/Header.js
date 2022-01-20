5e4ec8251baf82812dfa77f932f711a973a352b1
Oriol Serra <87442357+byoriolserra@users.noreply.github.com>
2022-01-20 18:31:00 +0100

    Make Header responsive for Desktop

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import hero from '../../assets/images/hero.svg';
 
 const Header = () => {
   return (
     <header>
-        <div id='#header-text'>
+        <img src={hero} alt='' />
+        <div id='header-text'>
           <h1>Your favourite tunes</h1>
           <h2>All <span role='img' aria-label='Sun emoji'>☀️</span> and all <span role='img' aria-label='Moon emoji'>🌙</span></h2>
         </div>
